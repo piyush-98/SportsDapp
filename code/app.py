1ddd7fed72ac4d431731ba3f6a77c10374ff5463
piyush-98 <arjunguptaa98@gmail.com>
2020-01-12 16:10:06 +0530

    completed q_gen

--- code/app.py
+++ code/app.py
@@ -19,22 +19,33 @@ import pyrebase
 class asknplay:
     def Q_generation(self):
         quiz={}
-        keywords=['How','many','will','take','give','match']
-        details={}
-        data=livematches(self)
-        for i in data["matches"]:
-        if i['header']['state']=='preview':
-            mid=i['match_id']
-            quiz["match_id"]=mid
-            quiz["curparticipation"]="1"
-            quiz["minq"]="12"
-            quiz["pfee"]="10"
-            quiz["q_array"]=[]
-            for j in range(20):
-                quiz[str(j)]=[]
-                
-        
+        def q_rand():
+            keywords=['How','many','will','take','give','match']
 
+        data=livematches(self)
+        for k in data["matches"]:
+            if k['header']['state']=='preview':
+                mid=k['match_id']
+                quiz["match_id"]=mid
+                quiz["curparticipation"]="1"
+                quiz["minq"]="12"
+                quiz["pfee"]="10"
+                quiz["q_array"]=[]
+                for i in range(21):
+                    quiz["q_array"][str(i)]={}
+                    que,q_type,options=q_rand()
+                    quiz["q_array"][str(i)]["content"]=que
+                    quiz["q_array"][str(i)]["type"]=q_type
+                    print(options)
+                    if options!=-1:
+                        quiz["q_array"][str(i)]["options"]={}
+                        for j in range(len(options)):
+                            quiz["q_array"][str(i)]["options"][str(j)]=options[j]
+                    else:
+                        quiz["q_array"][str(i)]["options"]=""
+                    
+        return quiz
+                
               
     def Data_make(self):
         columns=['q_id', 'Team','Over', 'Bowler', 'Batsmen', 'Bow_wickets',
